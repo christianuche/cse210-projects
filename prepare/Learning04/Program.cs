399cee8f06a3bd89aa4b8d4d2caddbee047ba990
Christian Uche <christianuche720@gmail.com>
2024-01-30 16:00:05 +0100

    Updated Homework

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Base "Assignment" object
+        Assignment A1 = new Assignment("Ogbuagu Obinna", "Multiplication");
+        Console.WriteLine(A1.GetSummary());
+
+        // Derived class assignments
+        MathAssignment A2 = new MathAssignment("Diana Ifegwu", "Fractions", "7.3", "8-19");
+        Console.WriteLine(A2.GetSummary());
+        Console.WriteLine(A2.GetHomeworkList());
+
+        WritingAssignment A3 = new WritingAssignment("Christian Uche", "Biafra History", "Nigeria Civil War");
+        Console.WriteLine(A3.GetSummary());
+        Console.WriteLine(A3.GetWritingInformation());
     }
 }
